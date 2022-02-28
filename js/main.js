ed2549f739da2e56ebe8b4420d8427e202f1e6a2
natasaghinadze <85290787+natasaghinadze@users.noreply.github.com>
2022-02-28 23:13:32 +0400

    add components & js

---
+++ js/main.js
@@ -0,0 +1,50 @@
+/*=============== LOADER ===============*/
+
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+    //   menu show 
+    // validate if constants exists
+    if(navToggle){
+        navToggle.addEventListener('click', () => {
+            navMenu.classList.add('show-menu')
+        })
+    }
+
+    // menu hidden
+    // validate if constant exists
+    if(navClose){
+        navClose.addEventListener('click', () =>{
+            navMenu.classList.remove('show-menu')
+        })
+    }
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__Link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // when we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader(){
+    const header = document.getElementById('header')
+    // when the scroll is greater then 50 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+/*=============== MIXITUP FILTER PRODUCTS ===============*/
+
+/* Default filter products */
+
+/* Link active products */
+
+/*=============== SHOW SCROLL UP ===============*/
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
